8df05312ec493cbcfedcef3426a7fda409fc4b5e
HL-CZ <145472767+HL-CZ@users.noreply.github.com>
2026-03-17 21:43:47 +0000

    Add navigation and authentication handling in App.vue

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,11 +1,68 @@
 <template>
-  <router-view />
+  <nav>
+   <router-link to = "/">home</router-link>|
+     <span> 
+        <router-link to="/Feed">Feed</router-link> |
+      </span>
+      <span v-if="isLoggedIn"> 
+        <button @click="handleSignOut">Logout</button> 
+      </span>
+      <span v-else>
+        <router-link to="/register"> Register </router-link> |
+        <router-link to="/login"> Login </router-link> |
+      </span>
+   <router-link to = "/about">about</router-link>|
+   <router-link to = "/More">More</router-link>
+   </nav>
+   <router-view/>
 </template>
+
+<script setup>
+import { ref, watchEffect } from 'vue' // used for conditional rendering
+import { getAuth,onAuthStateChanged, signOut } from 'firebase/auth'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const isLoggedIn = ref(true)
+
+// runs after firebase is initialized
+onAuthStateChanged(getAuth(),function(user) {
+    if (user) {
+      isLoggedIn.value = true // if we have a user
+    } else {
+      isLoggedIn.value = false // if we do not
+    }
+})
+
+const handleSignOut = () => {
+  signOut(getAuth())
+  router.push('/')
+}
+
+
+</script>
+
 
 <style>
 #app {
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
   font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 50px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
