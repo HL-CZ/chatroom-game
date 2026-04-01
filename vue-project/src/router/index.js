0e2c3b625748800e84b3273a005d0f4ad09c421e
HL-CZ <145472767+HL-CZ@users.noreply.github.com>
2026-04-01 01:55:36 +0100

    Remove unused routes from the router configuration

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -7,33 +7,6 @@ const routes = [
     name: 'home',
     component: HomeView
   },
-  {
-    path: '/about',
-    name: 'about',
-    redirect:"/about/us",  //重新定向
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
-    children: [
-      { 
-        //二级导航的路径不要加斜杠！！
-        path:"us",
-        component:() => import("../views/AboutSub/AboutUs.vue")
-      },
-      {
-        path:"info",
-        component: () => import("../views/AboutSub/Information.vue")
-      }
-     ]
-  },
-  {
-    path: '/More',
-    name: 'More',
-    component: () => import( /* webpackChunkName: "More" */ '../views/MoreView.vue')
-  },
-  {
-    path: '/News/:name',
-    name: 'News',
-    component: () => import(/* webpackChunkName: "details" */ '../views/News.vue')
-  },
         { path: '/Feed', component: () => import(/* webpackChunkName: "Feed" */ '../views/Feed.vue') },
         { path: '/register', component: ()  => import(/* webpackChunkName: "Register" */ '../views/Register.vue') },
         { path: '/login', component: () => import(/* webpackChunkName: "Login" */ '../views/Login.vue') },
